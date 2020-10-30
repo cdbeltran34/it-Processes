dde2c3f14080340650a356b773ce6926930d3c50
cdbeltran34 <cristian.1701422500@ucaldas.edu.co>
2020-10-30 12:03:59 -0500

    main con comunicacion stomp y log

--- main.py
+++ main.py
@@ -2,40 +2,110 @@ from Core.Modelos.Carpeta import Carpeta
 from Core.Controladores.ControladorCarpeta import ControladorCarpeta
 from Core.Controladores.ControladorSistema import Sistema
 import argparse
+import sys
+import stomp
+import time
+from stomp import *
+import threading
+from threading import Thread
+import json
+import traceback
+from datetime import datetime
 
-#Argumentos
-parser = argparse.ArgumentParser(
-    description='Esto es un programa que copia y cambia la extension de los archivos .T02 a .obs, .nav y .met ',
-)
-parser.add_argument('-o','--origen',metavar='',help='direccion carpeta origen entre comillas dobles')
-group =parser.add_mutually_exclusive_group()
-group.add_argument('-r','--ram',action='store_true',help='Ram del equipo')
-group.add_argument('-m','--ocu',action='store_true',help='Ram ocupada del equipo')
-group.add_argument('-d','--disp',action='store_true',help='Ram disponible del equipo')
-group.add_argument('-c','--cpu',action='store_true',help='cpu del equipo')
-args=parser.parse_args()
-#"./prueba/Manizales-49"
 
-#procesos
-carpeta=Carpeta(args.origen) 
+
+carpeta=Carpeta("./prueba/Manizales-49") 
 carpeta2=Carpeta("./prueba/MnaizalesGPS")
 controlador=ControladorCarpeta()
-controlador.copiarArchivos(carpeta,carpeta2)
-controlador.convertirArchivos(carpeta2)
-controlador.comprimirArchivosOBS(carpeta2)
-
-#informacion
 sistema=  Sistema()
-if args.ram:
-    print('Ram: {}'.format(sistema.mostrarRam())) 
-elif args.ocu:
-    print('Ram Ocupada: {}'.format(sistema.mostrarRamOcupada())) 
-elif args.disp:
-    print('Ram disp: {}'.format(sistema.mostrarRamDisponible())) 
-elif args.cpu:
-    print('Cpu: {}'.format(sistema.mostrarCpu())) 
 
+
+
+
+#metodo para crear el archivo log en formato json
+#parametro= dict(objeto diccionario con mensaje,fecha actual)
+def logging(dict):
+    loggingsFile='log.json'
+    with open(loggingsFile,'w')as f:
+        json.dump(dict,f,indent=4)
+#metodo para generar fecha y hora
+#retorna en formato UTC la fecha y hora del proceso
+def fecha():
+    return datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+
+
+#clase listener para captar el mensaje desde el broker stomp
+class MyListener(object):
+    def __init__(self, conn):
+        self.conn = conn
+    def on_error(self, headers, message):
+        print('received an error "%s"' % message)
+    def on_message(self, headers, message):
+        try:
+            print(5 / 0)
+        except ZeroDivisionError:
+            fullTraceback= str(traceback.format_exc())
+            
         
+
+            if message == "copiar":
+                
+                controlador.copiarArchivos(carpeta,carpeta2)
+                logging({'operacion': message,'fecha':fecha(),'traceback':fullTraceback})
+            elif message== "convertir":
+                controlador.convertirArchivos(carpeta2)
+                logging({'operacion': message,'fecha':fecha(),'traceback':fullTraceback})
+            elif message=="comprimir":
+                controlador.comprimirArchivosOBS(carpeta2)
+                logging({'operacion': message,'fecha':fecha(),'traceback':fullTraceback})
+            elif message=="todo":
+                t1=threading.Thread(target=controlador.copiarArchivos,args=(carpeta,carpeta2,))
+                t2=threading.Thread(target=controlador.convertirArchivos,args=(carpeta2,))
+                t3=threading.Thread(target=controlador.comprimirArchivosOBS,args=(carpeta2,))
+                t1.start()
+                t1.join()
+                t2.start()
+                t2.join()
+                t3.start()
+                t3.join()
+            elif message=="sistema":
+                print('Ram: {},Ram Ocupada: {},Ram disp: {},Cpu: {}'.format(sistema.mostrarRam(),sistema.mostrarRamOcupada()
+                ,sistema.mostrarRamDisponible(),sistema.mostrarCpu()))
+                logging({'operacion': message,'fecha':fecha(),'traceback':fullTraceback})
+            else:
+
+                print('Mensaje que no tiene operacion:  "%s"' % message)
+                self.conn.disconnect()
+
+#metodo para recibir el mensaje del broker stomp
+def recibirMensaje():
+    global mandar
+    mandar=True
+    #configuracion de parametros
+    conn = stomp.Connection()
+    conn.connect('admin', 'password', wait=True)
+    conn.set_listener('', MyListener(conn))
+    conn.subscribe(destination='/queue/hilo1', id=1, ack='auto')
+    print("Waiting for messages...")
+    time.sleep(100)
+    
+    #conn.disconnect()
+
+
+#metodo para responder el mensaje 
+
+def mandarMensaje():
+    global mandar
+    mandar=False
+    #configurar parametros de la cola
+    conn = stomp.Connection()
+    conn.connect('admin', 'password', wait=True)
+    #enviar el mensaje
+    conn.send(body='Listo para ejecutar', destination='/queue/hilo2')
+    conn.disconnect()
+
+recibirMensaje()
+mandarMensaje()
 
         
 
---
+++ publisher.py
@@ -0,0 +1,57 @@
+
+import time
+import sys
+import stomp
+import threading
+recibir=False
+
+
+def enviarMensaje():
+    global recibir
+    recibir=True
+    #configurar parametros de la cola
+    conn = stomp.Connection()
+    conn.connect('admin', 'password', wait=True)
+    #enviar el mensaje
+    conn.send(body=input("Mensaje a enviar: "), destination='/queue/hilo1')
+    #conn.disconnect()
+    
+
+
+#para imprimir el mensaje
+class MyListener(object):
+    def __init__(self, conn):
+        self.conn = conn
+    def on_error(self, headers, message):
+        print('received an error "%s"' % message)
+    def on_message(self, headers, message):
+        print('Mensaje:  "%s"' % message)
+        if message == "apagar":
+            print("entro al if de apagar")
+        self.conn.disconnect()
+
+def recibirMensaje():
+    global recibir
+    recibir=False
+    #configuracion de parametros
+    conn = stomp.Connection()
+    conn.connect('admin', 'password', wait=True)
+    conn.set_listener('', MyListener(conn))
+    conn.subscribe(destination='/queue/hilo2', id=1, ack='auto')
+    print("Waiting for messages...")
+    time.sleep(5)
+    conn.disconnect()
+    
+    
+
+
+
+enviarMensaje()
+
+recibirMensaje()
+
+
+
+
+     
+
